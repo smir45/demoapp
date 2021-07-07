8ce91ffec4c3d2a65d976949057b076eed9dfd3c
Samir Mishra <smir.mishra1551@gmail.com>
2021-07-07 12:40:51 +0545

    deployment

---
+++ precache-manifest.af258b60635706263988a7fb27e4bd8a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb855d853af1f655d7422b681ad40888",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a5c6a1c4d10d4bcdfd6",
+    "url": "/static/css/2.5dddc201.chunk.css"
+  },
+  {
+    "revision": "6136d88d93fcb04406d8",
+    "url": "/static/css/main.8853b0ad.chunk.css"
+  },
+  {
+    "revision": "8a5c6a1c4d10d4bcdfd6",
+    "url": "/static/js/2.0e4876af.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/static/js/2.0e4876af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6136d88d93fcb04406d8",
+    "url": "/static/js/main.b0cf05ae.chunk.js"
+  },
+  {
+    "revision": "99088a06eef5bb1d5717",
+    "url": "/static/js/runtime-main.69e36cf3.js"
+  },
+  {
+    "revision": "0661ee9c34e87aeb44e6640e3d97b236",
+    "url": "/static/media/aboutus.0661ee9c.svg"
+  },
+  {
+    "revision": "9d465b725a20c2f5e0999d14ca21532c",
+    "url": "/static/media/join.9d465b72.svg"
+  },
+  {
+    "revision": "dd8b58da0bbb5e89f2e0adc38218cbf1",
+    "url": "/static/media/signin2.dd8b58da.svg"
+  }
+]);
